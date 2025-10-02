62c42df8406ef1b6786e2f86082bb47be033e594
Hunter Isaacs <hsiman335@outlook.com>
2025-10-02 04:57:14 -0700

    attempting to fix the header for mobile

--- Info.js
+++ Info.js
@@ -1,6 +1,6 @@
 // Info.js
 // Central version and info for DiccChops pages
-export const DICCCHOPS_VERSION = 'v. 3.0.5';
+export const DICCCHOPS_VERSION = 'v. 3.0.6';
 export const DICCCHOPS_AD_TEXT = 'features, early access, and more.';
 export const DICCCHOPS_AD_LINK = '#';
 export const DICCCHOPS_AD_LINK_TEXT = 'Learn More';
