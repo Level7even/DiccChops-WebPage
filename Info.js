f195c8d13b8b473df27e4371a559caedd1988e67
Hunter Isaacs <hsiman335@outlook.com>
2025-10-02 15:59:04 -0700

    attempt 9999

--- Info.js
+++ Info.js
@@ -1,6 +1,6 @@
 // Info.js
 // Central version and info for DiccChops pages
-window.DICCCHOPS_VERSION = 'v. 3.0.7';
+window.DICCCHOPS_VERSION = 'v. 3.0.8';
 window.DICCCHOPS_AD_TEXT = 'features, early access, and more.';
 window.DICCCHOPS_AD_LINK = '#';
 window.DICCCHOPS_AD_LINK_TEXT = 'Learn More';
